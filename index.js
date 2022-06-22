1373558bd236733f00f98ec6b8a172cff616006a
Nick Cruz <nickcruz920@gmail.com>
2022-06-22 09:32:46 -0400

    Added getModals() function
    The getModals() Function will style multiple elements at once. Created a for loop to iterate through the nodelist that was created with document.getelementsbyclassname 
    
    Afterwards I styled some of the nodes differently to test things out for later use

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ function closeModal() {
   let modal = document.getElementById("modal");
   modal.style.display = "none";
 }
+
+function getModals() {
+  let manyModals = document.getElementsByClassName("m");
+ 
+  for(let i = 0; i < manyModals.length; i++)  {
+    manyModals[i].style.backgroundColor = "red" 
+    manyModals[i].innerHTML = "I am now red";
+    manyModals[i].style.marginTop = "10px";
+    manyModals[i].style.marginBottom = "10px";
+    
+    manyModals[2].style.marginBottom = "50px";
+    manyModals[2].style.backgroundColor = "green";
+    manyModals[2].innerHTML = "I am number 2";
+    manyModals[2].style.transition = "5s";
+    manyModals[2].style.transform = "rotate(50deg)";
+  
+    
+    
+    manyModals[3].style.marginLeft = "50px";
+    manyModals[3].innerHTML = "Three is me";
+    manyModals[3].style.backgroundColor = "blue";
+    manyModals[3].style.transform = "rotate(-50deg)";
+    manyModals[3].style.transition = "5s";
+  
+  }
+
+  
+}
